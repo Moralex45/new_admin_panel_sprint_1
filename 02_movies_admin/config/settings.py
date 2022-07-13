4b8cddf37ad99af89a766faf98d83d28251768c1
Aleksandr Morozov <moralex45@gmail.com>
2022-07-13 23:26:26 +0300

    Specifying the Secret_KEY parameter via the config

--- 02_movies_admin/config/settings.py
+++ 02_movies_admin/config/settings.py
@@ -30,7 +30,7 @@ include(
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f-d7z*bo-7abpfhkr14m+4k!zxp^)9t0g9g5o2hyo2&%@dgzqt'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False) == 'True'
