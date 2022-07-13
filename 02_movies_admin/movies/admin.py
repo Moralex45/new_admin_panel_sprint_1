5ab2125bc6421d3386e2d00796817390d358edd4
Aleksandr Morozov <moralex45@gmail.com>
2022-07-13 23:30:57 +0300

    Registration of models in the admin panel

--- 02_movies_admin/movies/admin.py
+++ 02_movies_admin/movies/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import Filmwork, Genre, GenreFilmwork, Person, PersonFilmwork
+
+class GenreFilmworkInline(admin.TabularInline):
+    model = GenreFilmwork
+
+class PersonFilmworkInline(admin.TabularInline):
+    model = PersonFilmwork
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description',)
+    search_fields = ('name', 'description', 'id',)
+
+
+@admin.register(Filmwork)
+class FilmworkAdmin(admin.ModelAdmin):
+    inlines = (GenreFilmworkInline,)
+    list_display = ('title', 'type', 'creation_date', 'rating',)
+    list_filter = ('type',)
+    search_fields = ('title', 'description', 'id')
+    list_prefetch_related = ('genres',)
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    search_fields = ('full_name', 'id', )
